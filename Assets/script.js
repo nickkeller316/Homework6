828a97f69cedec897bb856906a4102b28627e489
Nick Keller <nike0821@colorado.edu>
2020-12-15 21:07:55 -0800

    Editing html, getting key to display in console

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
+var apiKey = "fe33d085dc1bd41da9112f384d84bfff";
+var fullKey = requestUrl + apiKey;
+var searchButton = document.getElementById("search");
+console.log(fullKey);
